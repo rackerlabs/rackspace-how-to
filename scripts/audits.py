45f7e1b1e0e29f3410419a9a3e30e0b91acccbae
Kate Dougherty <kate.dougherty@rackspace.com>
2018-04-17 17:06:40 -0600

    Add script that retrieves H2 articles with audit dates that are within the provided date range.

--- scripts/audits.py
+++ scripts/audits.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import datetime
 import xlsxwriter
 
@@ -6,37 +7,40 @@ import xlsxwriter
 workbook = xlsxwriter.Workbook('../files/h2-audits.xlsx')
 worksheet = workbook.add_worksheet()
 
-info = ([])
+# Set global variables
 dir = "../content/"
+info = ([])
+first_arg = sys.argv[1]
+second_arg = sys.argv[2]
 
-def filter_files():
-    start_date = str(input("Begin date (use format YYYY-MM-DD): "))
-    end_date = str(input("End date (use format YYYY-MM-DD): "))
+
+def filter_files(start_date=first_arg, end_date=second_arg):
     r = []
     count = 0
     try:
         # convert the string arguments to date format
-        begin_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
-        stop_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+        start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+        end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
     except ValueError:
-        print("You didn't follow the right format.")
+        print('A problem occurred. Did you follow the correct date format?')
     # Loop through each file in the folder
     for root, dirs, files in os.walk(dir):
         for file in files:
             # construct the path for each file
             path = root + "/" + file
             # skip files that aren't relevant
-            if "all.md" in path or "index.md" in path or "retired-" in path or "DS_Store" in path or len(path) < 11:
+            if ("all.md" in path or "index.md" in path or "retired-" in path or
+                    "DS_Store" in path or len(path) < 11):
                 continue
             else:
-                #open the file and read the content
-                text = open(path,'r+')
+                # open the file and read the content
+                text = open(path, 'r+')
                 lines = text.readlines()
                 # skip files that don't have at least 3 lines
                 if len(lines) < 2:
                     continue
                 else:
-                    # set the audit_date variable (the 3rd list item in
+                    # set the audit_date variable (as the 3rd list item in
                     # "lines")
                     audit_date = lines[2]
                     # if the line at index 2 isn't a date, skip
@@ -44,34 +48,39 @@ def filter_files():
                         continue
                     else:
                         try:
-                            #convert the audit_date to a list so it can be
+                            # convert the audit_date to a list so it can be
                             # sliced
                             audit_date_list = list(audit_date)
                             # Slice audit_date_list to get chars at indexes
                             # 13-22
                             # only, then join those chars
                             audit_date_slice = "".join(audit_date_list[13:23])
-                            #convert audit_date_slice to date format
-                            audit_date_final = datetime.datetime.strptime(audit_date_slice, '%Y-%m-%d').date()
+                            # convert audit_date_slice to date format
+                            audit_date_final = \
+                                datetime.datetime.strptime(audit_date_slice,
+                                                           '%Y-%m-%d').date()
                         # if the index value is out of range, print the name
                         # of the offending file to get more info & correct
                         # the issue
                         except ValueError:
                             print("There's a problem with the audit date "
-                            " in the file at " + path)
+                                  " in the file at " + path)
                         # if the file's audit date is between the dates
                         # provided by the info dev, print the results
-                        if audit_date_final >= begin_date and audit_date_final <= stop_date:
-                            # Create the link
-                            # First, convert the file name to a list and strip the .md
-                            # file extension from it
+                        if audit_date_final >= start_date and \
+                           audit_date_final <= end_date:
+                            # Create the published link for the file
+                            # First, convert the file name to a list and strip
+                            # the .md file extension from it
                             file = list(file)
                             del file[-1:-4:-1]
-                            # Then, join the list items back together
+                            # Then, join the rest of the file name back
+                            # together
                             file = "".join(file)
 
-                            # Compose the link for each file
-                            link = "https://support.rackspace.com/how-to/" + file + "/"
+                            # Create the link for each file
+                            link = "https://support.rackspace.com/how-to/" \
+                                   + file + "/"
                             # Append the file name and link to "info"
                             info.append([audit_date_slice, path, link])
                             # Increment the counter
@@ -79,28 +88,30 @@ def filter_files():
                         else:
                             continue
     # Write file and link to a spreadsheet
-    # Start from the second cell. Rows and columns are zero
-    # indexed.
+    # Start from the second cell. Rows and columns are zero-indexed.
     row = 0
     col = 0
     # write file and link
-    try:
-         # Loop through the items inside the "info" *list of lists*
-         for grouping in (info):
-             worksheet.write(row, col, info[row][0])
-             worksheet.write(row, col + 1, info[row][1])
-             worksheet.write(row, col + 2, info[row][2])
-             row += 1
-    except:
-         print("An export problem occurred.")
+    # Loop through the items inside the "info" *list of lists*
+    for grouping in (info):
+        worksheet.write(row, col, info[row][0])
+        worksheet.write(row, col + 1, info[row][1])
+        worksheet.write(row, col + 2, info[row][2])
+        row += 1
     # print a success message
-    print("\nSuccess! There are {} How-To articles that fit your criteria. \n \n"
-    "YOUR RESULTS HAVE BEEN SAVED TO ..FILES/H2-AUDITS.XLSX. \n \n"
-    "To make this information editable for all Info Devs, copy and paste the "
-    "content into a new Excel file in O365 online and share it, then \n"
-    "** delete the local file at ../files/h2-audits.xlsx **.\n"
-    "\n".format(count))
+    print("\nSuccess! There are {} How-To articles that fit your criteria. "
+          " \n \n"
+          "YOUR RESULTS HAVE BEEN SAVED TO ..FILES/H2-AUDITS.XLSX. \n \n"
+          "To make this information editable for all Info Devs, copy and paste"
+          " the content into a new Excel file in O365 online and share it, "
+          "then \n"
+          "** delete the local file at ../files/h2-audits.xlsx **.\n"
+          "\n".format(count))
 
-# call the function
-filter_files()
+
+if __name__ == "__main__":
+    filter_files()
+
+
+# close the Excel Workbook
 workbook.close()
